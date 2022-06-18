627c5fec91f86c404b168ca94f8dd8b780c661ec
lbenvenutoc <lbenvenutoc@gmail.com>
2022-06-18 12:16:58 -0500

    fix interface

--- src/main/java/com/luisbenvenuto/Prueba.java
+++ src/main/java/com/luisbenvenuto/Prueba.java
@@ -4,8 +4,11 @@ public class Prueba {
 
     public static void main(String[] args) {
 
-        Calculadora calculadora = (Integer numero1, Integer numero2) -> numero1+numero2;
-        System.out.println(calculadora.suma(4,2));
+        Calculadora operacionSuma = (Integer numero1, Integer numero2) -> numero1+numero2;
+        System.out.println("Suma: "+operacionSuma.oepracion(4,2));
+
+        Calculadora operacionReesta = (Integer numero1, Integer numero2) -> numero1-numero2;
+        System.out.println("Resta: "+operacionReesta.oepracion(4,2));
 
     }
 
