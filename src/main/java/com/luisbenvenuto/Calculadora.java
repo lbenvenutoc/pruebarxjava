7a9b519c300d1a2ba7b37d29e7e326ee548e129d
lbenvenutoc <lbenvenutoc@gmail.com>
2022-06-18 11:34:29 -0500

    first commit

---
+++ src/main/java/com/luisbenvenuto/Calculadora.java
@@ -0,0 +1,7 @@
+package com.luisbenvenuto;
+
+public interface Calculadora {
+
+    Integer suma(Integer numero1, Integer numero2);
+
+}
---
+++ src/main/java/com/luisbenvenuto/Prueba.java
@@ -0,0 +1,29 @@
+package com.luisbenvenuto;
+
+import io.reactivex.Observable;
+
+public class Prueba {
+
+    public static void main(String[] args) {
+        //hello();
+        /*System.out.println(new Calculadora() {
+            @Override
+            public Integer suma(Integer numero1, Integer numero2) {
+                return numero1 + numero2;
+            }
+        }.suma(4,2));
+        */
+
+        Calculadora calculadora = (Integer numero1, Integer numero2) -> numero1+numero2;
+        System.out.println(calculadora.suma(4,2));
+
+
+    }
+
+    public static void hello() {
+        Observable.just("Hello, World!") // create new observable
+                .subscribe(onNext -> { // subscribe and perform action
+                    System.out.println(onNext);
+                });
+    }
+}
